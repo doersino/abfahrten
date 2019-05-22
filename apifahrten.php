632c8fefeb4a17368bb8d441bb1cf8593fe1064e
Noah Doersing <doersino@gmail.com>
2019-05-22 17:54:44 +0200

    In API request handler, check for presence instead of non-emptiness of fields (fixes "Invalid request" error when searching for "")

--- apifahrten.php
+++ apifahrten.php
@@ -4,26 +4,26 @@ require_once("core.php");
 
 function handleRequest($request) {
     // switch to html output function if desired
-    if (!empty($request["format"]) && $request["format"] == "html") {
+    if (isset($request["format"]) && $request["format"] == "html") {
         $format = encodeAsHTML;
     } else {
         $format = encodeAsJSON;
     }
 
-    if (!empty($request["id"])) {  // departures for given stop id
+    if (isset($request["id"])) {  // departures for given stop id
         $id = $request["id"];
         $departures = getDepartures($id);
         return $format("departures", $departures);
-    } else if (!empty($request["stop"])) {  // departures for given stop name and, if applicable, platform  // TODO untested
+    } else if (isset($request["stop"])) {  // departures for given stop name and, if applicable, platform  // TODO untested
         $stop = $request["stop"];
-        if (!empty($request["platform"])) {
+        if (isset($request["platform"])) {
             $details = getDetails(NULL, $stop, $request["platform"]);
         } else {
             $details = getDetails(NULL, $stop, NULL);
         }
         $departures = getDepartures($details["id"]);
         return $format("departures", $departures);
-    } else if (!empty($request["search"])) {  // stops matching given search string
+    } else if (isset($request["search"])) {  // stops matching given search string
         $search = $request["search"];
         $matches = findMatches($search);
         return $format("search", $matches);
